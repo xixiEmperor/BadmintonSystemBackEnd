82fd2642c3e79f2404247395f7af30ca52e04690
dd-no-dd <ddddcz@126.com>
2025-05-14 17:54:38 +0800

    用户注册登录

--- src/main/java/com/wuli/badminton/util/JwtUtil.java
+++ src/main/java/com/wuli/badminton/util/JwtUtil.java
@@ -3,7 +3,6 @@ package com.wuli.badminton.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
